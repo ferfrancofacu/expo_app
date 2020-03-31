3fd6910a990c661720971b12be4172dc73c1aab5
nicholasferreira <nicholas_sc@live.com>
2020-03-30 22:53:50 -0300

    button next page

--- src/view/CriarPasta.js
+++ src/view/CriarPasta.js
@@ -1,25 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { Block } from 'galio-framework'
 import Swiper from 'react-native-swiper';
+import { FAB } from 'react-native-paper';
+import theme from '../constants/ThemePaper'
 
-export default class App extends Component {
-  render() {
-    return (
-      <Swiper style={styles.wrapper} showsButtons>
-        <View style={styles.slide1}>
-          <Text style={styles.text}>Hello Swiper</Text>
-        </View>
-        <View style={styles.slide2}>
-          <Text style={styles.text}>Beautiful</Text>
-        </View>
+export default function CriarPasta(props) {
+  let swiper = null
+
+  const _onNextPage = () => {
+    swiper.scrollBy(1)
+  }
+
+  const _onPrevPage = () => {
+    swiper.scrollBy(-1)
+  }
+
+  return (
+    <Block flex>
+      <Swiper style={styles.wrapper}
+        showsPagination={false}
+        scrollEnabled={false}
+        index={1}
+        ref={e => swiper = e}>
         <View style={styles.slide3}>
           <Text style={styles.text}>And simple</Text>
         </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>aaa</Text>
+        </View>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Beautiful</Text>
+        </View>
       </Swiper>
-    );
-  }
+      <FAB
+        style={styles.fab}
+        color={'#fff'}
+        icon="chevron-right"
+        onPress={_onNextPage}
+      />
+    </Block>
+  )
 }
-
 const styles = StyleSheet.create({
   wrapper: {
   },
@@ -45,5 +67,12 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 30,
     fontWeight: 'bold'
-  }
+  },
+  fab: {
+    backgroundColor: theme.colors.primary,
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
 });
